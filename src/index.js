9b84afebac15f05e2049c8a34410c61ac6ef5fff
ziadhammad71 <ziedhammad71@gmail.com>
2025-03-19 18:03:35 +0200

    update commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
     const express = require('express');
-    //const mongoose = require('mongoose');
+    const mongoose = require('mongoose');
     const redis = require('redis');
-    const {Clinet, Client} = require('pg')
+    //const {Clinet, Client} = require('pg')
 
     // init app
     const PORT = process.env.PORT || 4000;
@@ -16,31 +16,31 @@
     redisClient.on('connect', () => console.log('connected to redis...'));
     redisClient.connect();
     //connect db
-    const DB_USER = 'root';
-    const PASSWORD_DB = 'example';
-    const PORT_DB = 5432;
-    const DB_HOST = "postgres"
-    const URI = `postgresql://${DB_USER}:${PASSWORD_DB}@${DB_HOST}:${PORT_DB}`;
-    const client = new Client({
-        connectionString: URI,
-    });
-    client
-        .connect()
-        .then(() => console.log('connected to postgres db...'))
-        .catch((err) => console.log('failed to connect to postgres db:', err));
-
-
-
-
-    //const USER_DB = 'root';
+    //const DB_USER = 'root';
     //const PASSWORD_DB = 'example';
-    //const PORT_DB = 27017;
-    //const DB_HOST = "mongo"
-    //const URI = `mongodb://${USER_DB}:${PASSWORD_DB}@${DB_HOST}:${PORT_DB}`;
-    //mongoose
-    //    .connect(URI)
-    //    .then(() => console.log('connected to DB'))
-    //    .catch((err) => console.log('failed to connect to DB:', err));
+    //const PORT_DB = 5432;
+    //const DB_HOST = "postgres"
+    //const URI = `postgresql://${DB_USER}:${PASSWORD_DB}@${DB_HOST}:${PORT_DB}`;
+    //const client = new Client({
+    //    connectionString: URI,
+    //});
+    //client
+    //    .connect()
+    //    .then(() => console.log('connected to postgres db...'))
+    //    .catch((err) => console.log('failed to connect to postgres db:', err));
+
+
+
+
+    const USER_DB = 'root';
+    const PASSWORD_DB = 'example';
+    const PORT_DB = 27017;
+    const DB_HOST = "mongo"
+    const URI = `mongodb://${USER_DB}:${PASSWORD_DB}@${DB_HOST}:${PORT_DB}`;
+    mongoose
+        .connect(URI)
+        .then(() => console.log('connected to DB'))
+        .catch((err) => console.log('failed to connect to DB:', err));
 
 
     app.get('/',(req , res) =>{
